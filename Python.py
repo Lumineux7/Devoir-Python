6951fcb728650869af71f5f91803c348129bfc8c
Frere Marc <ibindamarc@gmail.com>
2025-03-07 16:12:20 +0100

    Ajout du code pour l'initialiation des données

--- Python.py
+++ Python.py
@@ -1,2 +1,12 @@
 # Saisie du nombre d’étudiants
 n = int(input("Entrer le nombre d’étudiants : "))
+for i in range(n):
+    nom = input("Entrer le nom de l’étudiant : ")
+    n_notes = int(input(f"Entrer le nombre de notes pour {nom} : "))
+    
+    notes = []
+    for j in range(n_notes):
+        note = float(input(f"Entrer la note {j + 1} : "))
+        notes.append(note)
+    
+    etudiants[nom] = notes
